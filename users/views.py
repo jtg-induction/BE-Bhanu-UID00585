9673a5dd458692b9a5972d18508cb03bc1101ba0
bhanu2503 <bandaru.bhanuteja@joshtechnologygroup.com>
2025-04-01 12:38:59 +0530

    done with exercise10

--- users/views.py
+++ users/views.py
@@ -1,21 +1,25 @@
 from rest_framework import status
-
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
-from rest_framework.generics import CreateAPIView
+from django.contrib.auth.password_validation import validate_password 
+from rest_framework.generics import CreateAPIView,GenericAPIView
 
 
-from users.serializers import UserRegistrationSerializer 
+from users.serializers import UserLoginSerializer, UserRegistrationSerializer 
 
 class UserRegistrationAPIView(CreateAPIView):
   serializer_class=UserRegistrationSerializer
   permission_classes=[AllowAny]
 
 
+class UserLoginAPIView(GenericAPIView):
+    serializer_class=UserLoginSerializer
+    permission_classes=[AllowAny]
 
-class UserLoginAPIView:
-    """
-    success response format
-     {
-       auth_token: ""
-     }
-    """
+    def post(self,request):
+       serializer=self.get_serializer(data=request.data)
+       serializer.is_valid(raise_exception=True)
+       user=serializer.validated_data["user"]
+       token, _=Token.objects.get_or_create(user=user)
+       return Response({"auth_token":token.key},status=status.HTTP_200_OK)
