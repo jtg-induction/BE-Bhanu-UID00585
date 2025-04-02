7d43d655e246be073b34c730003056cbb92d8f49
bhanu2503 <bandaru.bhanuteja@joshtechnologygroup.com>
2025-04-02 16:43:41 +0530

    Ensured that all related tests are passing

--- users/tests.py
+++ users/tests.py
@@ -32,7 +32,7 @@ class UserRegistrationAPIViewTestCase(APITestCase):
             "confirm_password": "StrongPassword123!",
         }
         response = self.client.post(self.url, user_data)
-        self.assertEqual(201, response.status_code)
+        self.assertEqual(response.status_code, 201)
         self.assertTrue("token" in json.loads(response.content))
 
     def test_unique_email_validation(self):
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from .serializers import UserLoginSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.generics import CreateAPIView, GenericAPIView
 from users.serializers import UserLoginSerializer, UserRegistrationSerializer
