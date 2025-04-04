3d28c44494cfc4b8ab7964fc9a9d3c5aa453539a
bhanu2503 <bandaru.bhanuteja@joshtechnologygroup.com>
2025-04-04 22:27:12 +0530

    fixed the formting and linting changes

--- projects/serializers.py
+++ projects/serializers.py
@@ -33,9 +33,7 @@ class ProjectSerializerstartswithA(serializers.ModelSerializer):
         if obj.status == 2:
             return True
         else:
-            return False 
-        
-          
+            return False
 
 class ProjectReportSerializer(ProjectSerializer):
     """
